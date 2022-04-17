c8da5ba76f20002f46466eb17670cf00738288c2
JugolKarmakar <jugol.cse@gmail.com>
2022-04-17 22:53:58 +0600

    footer commit

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import logo from "../../../images/logo2.svg";
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
   return (
-    <div>
-      <h2>This</h2>
+    <div className="flex justify-around bg-blue-200 py-6 mt-10">
+      <div>
+        <img className="h-6" src={logo} alt="" />
+      </div>
+      <h2 className="text-black font-medium">
+        Copyright © {year} Swish Dental
+      </h2>
     </div>
   );
 };
